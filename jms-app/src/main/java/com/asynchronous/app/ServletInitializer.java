e9839c4da70b74bd62cd14ec3ccbe92e139dae5a
Jakub Pavlíček <kuba.cb@seznam.cz>
2025-10-23 13:14:34 +0200

    Make WildFly work

--- jms-app/src/main/java/com/asynchronous/app/ServletInitializer.java
+++ jms-app/src/main/java/com/asynchronous/app/ServletInitializer.java
@@ -1,11 +1,13 @@
 package com.asynchronous.app;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
+    @NonNull
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(JmsAppApplication.class);
     }
