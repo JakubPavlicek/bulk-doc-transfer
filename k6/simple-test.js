18895dd440c878b347393ab74b5e6bf973884f5d
DESKTOP-MQFNBUR\milan <milan.janoch@gmail.com>
2025-11-09 14:30:00 +0100

    test with multiple files prepared

--- k6/simple-test.js
+++ k6/simple-test.js
@@ -1,30 +1,42 @@
 import http from "k6/http";
 import { check } from "k6";
+import { FormData } from "https://jslib.k6.io/formdata/0.0.2/index.js";
 
 const txtManifest = JSON.parse(open("data/txt/manifest.json"));
 const smallestTxtFile = txtManifest.files.find((f) => f.category === "1");
+const smallestTxtFile2 = txtManifest.files.find((f) => f.category === "2");
 const txtFileData = open(`data/txt/${smallestTxtFile.name}`, "b");
+const txtFileData2 = open(`data/txt/${smallestTxtFile2.name}`, "b");
 
 export const options = {
   vus: 10,
-  iterations: 100,
+  iterations: 10,
 };
 
-const BASE_URL = "http://localhost:8020";
+const BASE_URL = "http://localhost:8010";
 
 export default function () {
-  const data = {
-    email: "jpvlck@students.zcu.cz",
-    subject: "Single file test",
-    description: "Testing with just one file.",
-    files: http.file(txtFileData, smallestTxtFile.name),
-  };
+  const fd = new FormData();
+  fd.append("email", "jpvlck@students.zcu.cz");
+  fd.append("subject", "Single file test");
+  fd.append("description", "Testing with just one file.");
+  fd.append(
+    "files",
+    http.file(txtFileData, smallestTxtFile.name, "text/plain")
+  );
+  fd.append(
+    "files",
+    http.file(txtFileData2, smallestTxtFile2.name, "text/plain")
+  );
 
-  console.log("data prepares", data);
+  console.log("FormData prepared");
+  console.log(
+    `Uploading files: ${smallestTxtFile.name}, ${smallestTxtFile2.name}`
+  );
 
-  console.log(`Uploading single file: ${smallestTxtFile.name}`);
-
-  const response = http.post(`${BASE_URL}/api/v1/submissions`, data);
+  const response = http.post(`${BASE_URL}/api/v1/submissions`, fd.body(), {
+    headers: { "Content-Type": "multipart/form-data; boundary=" + fd.boundary },
+  });
 
   console.log(`Response status: ${response.status}`);
   console.log(`Response body: ${response.body}`);
